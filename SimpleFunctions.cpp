772324a2a93c218a92fc4a883ef7d8b753344498
CrZ <81176282+David123car7@users.noreply.github.com>
2024-09-28 20:24:45 +0100

    Create SimpleFunctions.cpp

---
+++ SimpleFunctions.cpp
@@ -0,0 +1,21 @@
+/*
+Returns the player velocity
+*/
+float AFirstPersonController::GetPlayerVelocity() {
+	FVector velocity = GetVelocity();
+	return velocity.Length();
+}
+
+/*
+Returns the height of the capsuleComponent 
+ */
+float AFirstPersonController::GetPlayerHeight() {
+	return GetCapsuleComponent()->GetScaledCapsuleHalfHeight();
+}
+
+/*
+Returns the APlayerController of the player
+*/
+APlayerController* AFirstPersonController::GetPlayerController() {
+	return Cast<APlayerController>(GetController());
+}
