f7db6bad980b850736bbc70f75bf7d69a7b94d6c
CrZ <81176282+David123car7@users.noreply.github.com>
2024-10-06 18:48:05 +0100

    Update VariousImplementations.cpp
    GetPlayerSurface()

--- VariousImplementations.cpp
+++ VariousImplementations.cpp
@@ -26,3 +26,26 @@ int AFirstPersonController::Raycast() {
 	return -1; //If returns -1 means the raycast was not casted (it will probably never happen if things are working well)
 }
 
+/*
+This function returns the surface which player currently stands
+
+Can be used for example to create footsteps sounds, changing the sounds when the player changes surface (from dirt to sand for example).
+Docs to the creation of the UPhysicalMaterial: https://dev.epicgames.com/documentation/en-us/unreal-engine/physical-materials-user-guide-for-unreal-engine?application_version=5.4
+*/
+EPhysicalSurface AFirstPersonController::GetPlayerSurface() {
+	FHitResult hitResult;
+	FVector startTrace = GetCapsuleComponent()->GetComponentLocation();
+	FVector downVector = GetCapsuleComponent()->GetUpVector();
+	FVector endTrace = (downVector * -1000.f) + startTrace; //5000 is range of the ray
+	FCollisionQueryParams* CQP = new FCollisionQueryParams();
+	CQP->bReturnPhysicalMaterial = true;
+
+	if (GetWorld()->LineTraceSingleByChannel(hitResult, startTrace, endTrace, ECC_Visibility, *CQP)) { 
+		DrawDebugLine(GetWorld(), startTrace, endTrace, FColor(255, 0, 0), true);
+		auto physMat = hitResult.PhysMaterial;
+		if (physMat != NULL)
+			return UGameplayStatics::GetSurfaceType(hitResult);
+	}
+
+	return SurfaceType_Default; 
+}
